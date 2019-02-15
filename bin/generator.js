fdc8e43412f4e48e5576ec7619904bc57f074e0b
chenjingnan <chenjingnan@jd.com>
2019-02-15 18:14:08 +0800

    修改

--- bin/generator.js
+++ bin/generator.js
@@ -2,12 +2,8 @@
 
 'use strict';
 
-const co = require('co');
 const Command = require('..');
 
-co(function* () {
-  yield new Command().run(process.cwd(), process.argv.slice(2));
-}).catch(err => {
-  console.error(err.stack);
-  process.exit(1);
-});
+
+new Command().run(process.cwd(), process.argv.slice(2));
+
